f95310a94ddb1a741a568deb3f665fdc622229d6
lwod <81166558+lwod@users.noreply.github.com>
2021-03-24 10:40:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 5000
 
-app.use(express.json({extended:true));
+app.use(express.json({extended:true}));
 
 app.get("/", function (req,res){
 	res.send("some str");

76394b0184b98cd960b25ec854243f51a76894aa
lwod <81166558+lwod@users.noreply.github.com>
2021-03-24 10:43:42 +0200

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,14 @@ app.get("/test", async (req,res)=>{
 	res.sendFile(`${__dirname}/index.html`)
 })
 
+app.post("\test", async (req,res)=>{
+
+	let var1 = req.body.var1;
+	let var2 = req.body.var2;
+	
+	console.log(`var1: ${var1} and var2: ${var2}`);
+});
+
 app.listen(process.env.PORT || 5000, ()=>{
 	console.log(`\tstratet on port: ${port}`)
 })
